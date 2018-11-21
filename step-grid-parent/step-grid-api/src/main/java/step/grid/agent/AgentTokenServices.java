8980ec53b568601d7d16a613409e22fccbd63fd2
Jérôme Comte <jerome.comte@exense.ch>
2018-11-21 13:23:23 +0100

    Fixing copyrights

--- step-grid-parent/step-grid-api/src/main/java/step/grid/agent/AgentTokenServices.java
+++ step-grid-parent/step-grid-api/src/main/java/step/grid/agent/AgentTokenServices.java
@@ -1,12 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * (C) Copyright 2018 Jerome Comte and Dorian Cransac
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
+ * This file is part of STEP
+ *
+ * STEP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * STEP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with STEP.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 package step.grid.agent;
 
