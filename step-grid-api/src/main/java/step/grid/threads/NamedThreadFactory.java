218538e6db00c3d5b21be39578c357f0210c2220
Christoph Langguth <christoph.langguth@exense.ch>
2025-10-02 09:29:25 +0200

    SED-2866 keyword output streaming (#43)
    Co-authored-by: Jérôme Comte <jerome.comte@exense.ch>

--- step-grid-api/src/main/java/step/grid/threads/NamedThreadFactory.java
+++ step-grid-api/src/main/java/step/grid/threads/NamedThreadFactory.java
@@ -5,22 +5,32 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NamedThreadFactory implements ThreadFactory {
+    private final boolean daemon;
     private final String baseName;
     private final AtomicInteger count = new AtomicInteger(0);
     private static final ConcurrentHashMap<String, AtomicInteger> countByName = new ConcurrentHashMap<>();
 
-    protected NamedThreadFactory(String baseName) {
+    protected NamedThreadFactory(String baseName, boolean daemon) {
         this.baseName = baseName;
+        this.daemon = daemon;
     }
 
+
+    @Deprecated // prefer the variant with explicit daemon parameter to make behavior clear
     public static NamedThreadFactory create(String baseName) {
-        return new NamedThreadFactory(baseName);
+        return create(baseName, false);
+    }
+
+    public static NamedThreadFactory create(String baseName, boolean daemon) {
+        return new NamedThreadFactory(baseName, daemon);
     }
 
     @Override
     public Thread newThread(Runnable r) {
-        return new Thread(r, baseName + "-" +
+        Thread t = new Thread(r, baseName + "-" +
                 countByName.computeIfAbsent(baseName, n -> new AtomicInteger(0)).getAndIncrement() + "-"
                 + count.getAndIncrement());
+        t.setDaemon(daemon);
+        return t;
     }
 }
